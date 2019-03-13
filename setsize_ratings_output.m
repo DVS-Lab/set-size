8d3eb96eff4de0d75a9ba81f16c206d09da1d88a
Victoria Kelly <35232563+victoriaakelly@users.noreply.github.com>
2019-03-13 11:41:58 -0400

    Update setsize_ratings_output.m

--- setsize_ratings_output.m
+++ setsize_ratings_output.m
@@ -8,9 +8,9 @@ ChoiceTask = dir('*_task_b_results.csv');
 % open output files
 fname = fullfile(maindir,'setsize_ratings_output.csv');
 fid_run = fopen(fname,'w'); % csv uses commans (,) & tsv uses tabs (\t)
-fprintf(fid_run,'subject_id,INSERTSTUFFHERE\n');
+fprintf(fid_run,'snack_item,choosing_for_self,choosing_for_partner,\n');
 
-sublist = [102 109 110 111 113 115 117 118 119 120 121 122 123 124 125 126 127 128 129 130 131 132 133 134];
+sublist = [103 109 117];
 
 % pulling from Ratings Task--------------------------------------------------------------------------------
 for s = 1:200(RatingsTask)
@@ -25,9 +25,14 @@ for s = 1:200(RatingsTask)
     
     trial_data = '';
     
-    % put more here
+    % define snack_item
+    for i = 1:200(C{1})
+        snack_item = % pull snack name from file namme (...\JOCN-master\task a\images\SNACKNAMES.jpg)
+        % list each snack item in each row of the first column
     
-
+    % once each snack has been listed:
+    for i = 1:200(C{2})
+        % take averages of each snack item pre-rating by looking at the second column of the Task A files for each participant
 end
 
 % pulling from Choice Task---------------------------------------------------------------------------------
@@ -43,6 +48,19 @@ for s = 1:length(ChoiceTask)
 
     trial_data = '';
 
-
+    % define ratings means
+    choosing_for_self_means = [];
+    choosing_for_partner_means = [];
+    
+    % pull choosing_for_self ratings data
+    for i = 1:200(C{10})
+        if isequal(C{12})(i),{'0'} % non-compuer responses
+            if isequal(C{1}(i),{}) % trial types 
+        % look for specific snack item
+        % pull ratings data from C{10} and take averages across all participants
+        
+    
+    % pull choosing_for_partner ratings data
+    
 
 end
