79ee9a5b348ca3688fcaa187d51bcd8c18acf0a4
Victoria Kelly <35232563+victoriaakelly@users.noreply.github.com>
2019-04-04 21:13:02 -0400

    code in progress

--- setsize_ratings_output.m
+++ setsize_ratings_output.m
@@ -13,7 +13,7 @@ sublist = [102 109 117];
 baseoutput = fullfile(maindir,'output');
 
 % open output files
-fname = fullfile(maindir,'setsize_ratings_output.csv');
+fname = fullfile(maindir,'setsize_ratings_output10.csv');
 fid_run = fopen(fname,'w'); % csv uses commas (,) & tsv uses tabs (\t)
 fprintf(fid_run,'snackNames,preRatings_means,you_means,partner_means\n');
 
@@ -27,7 +27,7 @@ for s = sublist
     taskAFile = dir(fullfile(baseoutput,[num2str(s) '_task_a_results.csv']));
     taskBFile = dir(fullfile(baseoutput,['subject_' num2str(s) '_partner*_task_b_results.csv']));
     
-% get PreRatings
+    % get PreRatings
     subjectPreRatings = [];
     subjectYouRatings = [];
     subjectPartnerRatings = [];
@@ -40,14 +40,14 @@ for s = sublist
     for i = 1:length(snackNames)
         snackRating = [];
         for j = 1:length(taskAData{1,1})
-           [~,snackName,~] = fileparts(taskAData{1, 1}{j, 1});
+           [~,snackName,~] = fileparts(taskAData{1}{j});
            if isequal(snackNames{i},snackName)
-              snackRating = [snackRating, taskAData{1, 2}(j)];
+              snackRating = [snackRating, taskAData{2}(j)];
            end
         end
-        subjectPreRatings = [subjectPreRatings,mean(snackRating)];
+        subjectPreRatings = [subjectPreRatings,snackRating];
     end
-    preRatings_means = [preRatings_means,subjectPreRatings];
+    preRatings_means = [preRatings_means;subjectPreRatings];
     
     % get PostRatings
     fname = taskBFile.name;
@@ -61,26 +61,38 @@ for s = sublist
         for j = 1:length(taskBData{1,1})
            [~,snackName,~] = fileparts(taskBData{1, 7}{j, 1});
            if isequal(snackNames{i},snackName)
-               if isequal(taskBData{12}(i),{'0'}) % non-computer responses
-                  if taskBData{1,13}{j,1} == '1' % rating for yourself
-                    rating_You = [rating_You, str2num(taskBData{1, 10}{j, 1})];
+               if isequal(taskBData{12}{j},'0') % non-computer responses
+                  if taskBData{13}{j} == '1' % rating for yourself
+                    rating_You = [rating_You, str2num(taskBData{10}{j})];
                   else % rating for your partner
-                    rating_Partner = [rating_Partner, str2num(taskBData{1, 10}{j, 1})];
+                    rating_Partner = [rating_Partner, str2num(taskBData{10}{j})];
                   end
                end
            end
         end
-        subjectYouRatings = [subjectYouRatings,mean(rating_You)];
-        subjectPartnerRatings = [subjectPartnerRatings,mean(rating_Partner)];
+        subjectYouRatings = [subjectYouRatings,nanmean(rating_You)];
+        subjectPartnerRatings = [subjectPartnerRatings,nanmean(rating_Partner)];
     end
-    you_means = [you_means,subjectYouRatings];
-    partner_means = [partner_means,subjectPartnerRatings];
+    you_means = [you_means;subjectYouRatings];
+    partner_means = [partner_means;subjectPartnerRatings];
 end
 
+% format data
+snackNames = snackNames(1:end-1)';
+you_means = you_means(:,1:end-1);
+partner_means = partner_means(:,1:end-1);
+
+% get means across all subjects
+preRatings_means = nanmean(preRatings_means)';
+you_means = nanmean(you_means)';
+partner_means = nanmean(partner_means)';
+
 % write in tmp_data
-tmp_data = [snackNames preRatings_means you_means partner_means];
+tmp_data = [snackNames, num2cell(preRatings_means), num2cell(you_means), num2cell(partner_means)];
+
+% cell2csv('testoutput.csv',tmp_data);
 
 % write data to output file 'setsize_ratings_output.tsv'
-fprintf(fid_run,'%s,%s,%s,%s\n',tmp_data{:});
+fprintf(fid_run,'%s,%f,%f,%f\n',tmp_data{:});
 
 fclose(fid_run);
