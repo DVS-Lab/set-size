24d02ece7e49ada4143898133628e541788e0018
Victoria Kelly <35232563+victoriaakelly@users.noreply.github.com>
2019-04-09 16:21:32 -0400

    code still in progress

--- setsize_ratings_zScores.m
+++ setsize_ratings_zScores.m
@@ -7,26 +7,25 @@ fprintf(fid_run,'subj_id,zScore\n');
 
 sublist = [102 109 110 113 115 117 118 119 120 121 122 123 124 125 126 127 128 131 132 135 136 137 138 139 140];
 baseoutput = fullfile(maindir,'output');
-
-% get participant output files
-for s = sublist
-    taskAFile = dir(fullfile(baseoutput,[num2str(s) '_task_a_results.csv']));
-    myFiles = dir('*results.csv');
-end
-
-% get participant output files
-for s = 1:length(myFiles)
-    subj_id = sublist(s);
-    file_name = myFiles(s).name;
-    fname = fullfile(maindir,file_name);
-    fid = fopen(fname);
     
-    % defines what "C" (column) is from data output file
-    C = textscan(fopen(fname,'r'),'%s%d%d','Delimiter',',','HeaderLines',1);
+% get participant output files
+for s = 1:length(sublist)
+    taskAFile = dir(fullfile(baseoutput,[num2str(sublist(s)) '_task_a_results.csv']));
+    
+    % get participant output files
+    subj_id = sublist(s);
+    fname = taskAFile(s).name;
+    fid = fopen(fname);
+    taskAData = textscan(fopen(fname,'r'),'%s%d%d','Delimiter',',');
+    taskAData = cell2mat(taskAData(2));
     fclose(fid);
-
+    
+    % pull satisfaction ratings
+    ratings = double(taskAData);
+    
     % get zScore
-    zScore = zscore(taskAFile{2});
+    zScore = zscore(ratings);
+    zScore = mean(zScore);
     
     % write in tmp_data
     tmp_data = [subj_id zScore];
@@ -34,4 +33,4 @@ for s = 1:length(myFiles)
     % write data to output file 'setsize_choice1rt_output.tsv'
     fprintf(fid_run,'%d,%s\n',tmp_data);
 end
-close(fid_run);
+fclose(fid_run);
