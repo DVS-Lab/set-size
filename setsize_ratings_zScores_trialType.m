051792cd42e08935e4620e7cdf1514bd967d02b1
Victoria Kelly <35232563+victoriaakelly@users.noreply.github.com>
2019-04-23 13:54:38 -0400

    Working version w/ preRatings
    Still need to add another column. This column will be the difference between post and pre ratings.

--- setsize_ratings_zScores_trialType.m
+++ setsize_ratings_zScores_trialType.m
@@ -12,6 +12,16 @@ sublist = [102 109 110 113 115 117 118 119 120 121 122 123 124 125 126 127 128 1
 for s = 1:length(sublist)
     % Get subject ID
     subj_id = sublist(s);
+    
+    % Get task a data
+    task_a_file = dir(fullfile(output_folder_path,...
+        [num2str(subj_id) '_task_a_results_z_scores.csv']));
+
+    file_name = task_a_file.name;
+    fid_task_a = fopen(file_name);
+    task_a_data = textscan(fopen(file_name,'r'),'%s%s','Delimiter',',');
+    fclose(fid_task_a);
+    
     % Generate needed file name based on subj_id
     taskBFile = dir(fullfile(output_folder_path,...
         ['subject_' num2str(subj_id) '_partner*_task_b_results.csv']));
@@ -22,7 +32,7 @@ for s = 1:length(sublist)
     % Put data into cell struct
     task_b_data = textscan(fopen(fname,'r'),'%s%s%s%s%s%s%s%s%s%s%s%s%s','Delimiter',',');
     % Get relevant data
-    snack_names = task_b_data(7);
+    choice_2 = task_b_data(7);
     ratings = task_b_data(10);
     chosing_for = task_b_data(13);
     pc_response = task_b_data(12);
@@ -30,7 +40,7 @@ for s = 1:length(sublist)
     choice1 = task_b_data(5);
     % Close task B data file
     fclose(input_file);
-
+    
     % Format ratings to become double array
     ratings_temp = cellfun(@str2double,ratings{1:end},'UniformOutput',false);
     ratings_formatted = cell2mat(ratings_temp);
@@ -42,23 +52,39 @@ for s = 1:length(sublist)
     chosing_for_formatted = cell2mat(chosing_for_temp);
     pc_response_temp = cellfun(@str2double,pc_response{1:end},'UniformOutput',false);
     pc_response_formatted = cell2mat(pc_response_temp);
+    
+    % Get Pre Ratings
+    subject_pre_ratings = [];
+    for i = 1:length(choice_2{1}) % Iterate through all choices
+       ind = find(ismember(task_a_data{1},choice_2{1}{i}));
+       if length(ind) < 1 % choice is current snack
+          subject_pre_ratings = [subject_pre_ratings, NaN];
+       else
+          subject_pre_ratings = [subject_pre_ratings, str2double(task_a_data{2}{i})];
+       end
+    end
+    subject_pre_ratings = subject_pre_ratings'; % Put in column
     % Format data to be written
-    formatted_data = ...
-        [TrialType{1:end} snack_names{1:end} choice1{1:end} num2cell(z_scores)...
+    formatted_data =...
+        [TrialType{1:end} choice_2{1:end} choice1{1:end}...
+        num2cell(subject_pre_ratings) num2cell(z_scores)...
         num2cell(chosing_for_formatted) num2cell(pc_response_formatted)];
-
+    
     % Open output file
     % (file name is same as input file name but with '_formatted' appended)
     fname = fullfile(maindir,[taskBFile.name(1:end-4) '_z_scores_tt.csv']);
     output_file = fopen(fname,'w'); % csv uses commas (,) & tsv uses tabs (\t)
     % Write File Header
-    fprintf(output_file,'TrialType,snackName,choice1,rating,choseFor,pcResponse\n');
+    fprintf(output_file,'TrialType,choice2,choice1,preRating,rating,choseFor,pcResponse\n');
     
     % write data line by line
     for i = 2:length(formatted_data)
-        fprintf(output_file,'%s,%s,%s,%f,%d,%d\n',formatted_data{i,:});
+        if strlength(formatted_data{i,2}) > 10
+            fprintf(output_file,'%s,%s,%s,%f,%f,%d,%d\n',formatted_data{i,:});
+        end
     end
     
     % Close output file
     fclose(output_file);
 end
+fclose('all');
